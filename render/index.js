c193d624c5e5f7e759311fd51d0a2d5e45ed42f5
Polly <973367375@qq.com>
2018-02-26 14:56:46 +0800

    update

--- render/index.js
+++ render/index.js
@@ -1,15 +1,28 @@
-// inde
+// --registry=https://registry.npm.taobao.org
 const mongoose = require('mongoose')
 const electron = require('electron')
+const $ = require('jquery')
 const Store = require('./model')
-const remote = electron.remote
-const TweenMax = window.TweenMax
-const pText = $('.process-text')
+const { remote, clipboard } = electron
 
 const shareStore = {
     docs: []
 }
 
+const loadContent = document.querySelector('.loading-content')
+const body = document.querySelector('body')
+const mainContent = document.querySelector('.main-content')
+const ul = document.querySelector('ul')
+const next = document.querySelector('#next')
+const pText = $('.process-text')
+const $tpages = $('.totalpages')
+const $page = $('#page')
+const $btn = $('button,#page')
+
+const $loadingHint = $('.loading-hint')
+const $copyHint = $('.copy-hint')
+
+let totals = 0
 let currentPage = 0
 
 function connectMongo(excute) {
@@ -28,20 +41,13 @@ function connectMongo(excute) {
         excute(db)
     })
 }
-const loadContent = document.querySelector('.loading-content')
-const body = document.querySelector('body')
-const mainContent = document.querySelector('.main-content')
-const ul = document.querySelector('ul')
-const next = document.querySelector('#next')
-const copy = document.querySelector('#copytext')
-const $tpages = $('.totalpages')
-const $page = $('#page')
-const $btn = $('button')
 
-const $loadingHint = $('.loading-hint')
-const $copyHint = $('.copy-hint')
+// search for localStorage
+currentPage = Number(localStorage.getItem('pages'))
+if (currentPage === null) {
+    currentPage = 0
+}
 
-let totals = 0
 
 pText.text('正在链接服务器...')
 const vm = new window.Vue({
@@ -54,30 +60,9 @@ const vm = new window.Vue({
 connectMongo(function (db) {
     const win = remote.getCurrentWindow()
     pText.text('正在获取资源...')
-    getBundles(0).then((res) => {
-        res.docs.forEach(element => {
-            const $body = $(element.info)
-            const p = $body.find("p")
-            const star = $body.find('.star-name>a')
-            let htmlStr = ''
-            element.info = ''
-            totals = res.total / 15
-            $tpages.text(`共${Math.ceil(totals)}頁`)
-
-            for (let i = 0; i < 3; i++) {
-                element.info += p[i].outerHTML.replace(/style=\"[\s\S]*?\"/ig, '')
-            }
-            if (star.length) {
-                star.each((index, ele) => { htmlStr += `<span> ${ele.innerHTML} </span>` })
-            } else {
-                htmlStr = ` <span> 未知 </span>`
-            }
-            element.info += `<p><span>演員:</span>${htmlStr}</p>`
-            element.magnet = element.magnet.replace(/window\.open\(\'([\s\S]*?)\',\'_self\'\)/ig, function (matched, $1) { return `copyText('${$1}')` })
-            console.log(element.magnet)
-            vm.docs.push(element)
-            $page.get(0).value = (currentPage + 1)
-        });
+    getBundles(currentPage).then((res) => {
+        composeResource(res)
+        $page.get(0).value = currentPage + 1
         body.removeChild(loadContent)
     })
     win.on('closed', function () {
@@ -90,32 +75,13 @@ connectMongo(function (db) {
         currentPage--
         getBundles(currentPage * 15).then((res) => {
             vm.docs.length = 0
-            res.docs.forEach((element, index) => {
-                const $body = $(element.info)
-                const p = $body.find("p")
-                const star = $body.find('.star-name>a')
-                let htmlStr = ''
-                element.info = ''
-                for (let i = 0; i < 3; i++) {
-                    p[i].removeAttribute('style')
-                    element.info += p[i].outerHTML
-                }
-                if (star.length) {
-                    star.each((index, ele) => { htmlStr += `<span> ${ele.innerHTML} </span>` })
-                } else {
-                    htmlStr = ` <span> 未知 </span>`
-                }
-                element.info += `<p><span>演員:</span>${htmlStr}</p>`
-                element.magnet = element.magnet.replace(/window\.open\(\'([\s\S]*?)\',\'_self\'\)/ig, function (matched, $1) { return `copyText('${$1}')` })
-                vm.docs.push(element)
-                $btn.prop('disabled', false)
-                $loadingHint.addClass('hidden')
-                $page.get(0).value = (currentPage + 1)
-                window.scroll(0, 0);
-            });
+            composeResource(res)
+            $page.get(0).value = (currentPage + 1)
+            setValue()
         })
     })
     next.addEventListener('click', function (e) {
+        if (currentPage + 1 > totals) return
         const self = this
         $loadingHint.removeClass('hidden')
         $btn.prop('disabled', true)
@@ -123,63 +89,22 @@ connectMongo(function (db) {
         getBundles(currentPage * 15).then((res) => {
             console.log('fetched')
             vm.docs.length = 0
-            res.docs.forEach((element, index) => {
-                const $body = $(element.info)
-                const p = $body.find("p")
-                const star = $body.find('.star-name>a')
-                let htmlStr = ''
-                element.info = ''
-                for (let i = 0; i < 3; i++) {
-                    p[i].removeAttribute('style')
-                    element.info += p[i].outerHTML
-                }
-                if (star.length) {
-                    star.each((index, ele) => { htmlStr += `<span> ${ele.innerHTML} </span>` })
-                } else {
-                    htmlStr = `<span> 未知 </span>`
-                }
-                element.info += `<p><span>演員:</span>${htmlStr}</p>`
-                element.magnet = element.magnet.replace(/window\.open\(\'([\s\S]*?)\',\'_self\'\)/ig, function (matched, $1) { return `copyText('${$1}')` })
-                vm.docs.push(element)
-                $btn.prop('disabled', false)
-                $page.get(0).value = (currentPage + 1)
-                $loadingHint.addClass('hidden')
-                window.scroll(0, 0);
-            });
+            composeResource(res)
+            $page.get(0).value = (currentPage + 1)
+            setValue()
         })
     })
     $page.on('keyup', e => {
         if (e.keyCode !== 13) return
         const page = Number($page.get(0).value)
-        if (!isNaN(page) && page <= totals) {
+        if (!isNaN(page) && page <= totals && page > 0) {
             $loadingHint.removeClass('hidden')
             $btn.prop('disabled', true)
             getBundles((page - 1) * 15).then((res) => {
-                console.log('fetched')
                 vm.docs.length = 0
-                res.docs.forEach((element, index) => {
-                    const $body = $(element.info)
-                    const p = $body.find("p")
-                    const star = $body.find('.star-name>a')
-                    let htmlStr = ''
-                    element.info = ''
-                    for (let i = 0; i < 3; i++) {
-                        p[i].removeAttribute('style')
-                        element.info += p[i].outerHTML
-                    }
-                    if (star.length) {
-                        star.each((index, ele) => { htmlStr += `<span> ${ele.innerHTML} </span>` })
-                    } else {
-                        htmlStr = `<span> 未知 </span>`
-                    }
-                    element.info += `<p><span>演員:</span>${htmlStr}</p>`
-                    element.magnet = element.magnet.replace(/window\.open\(\'([\s\S]*?)\',\'_self\'\)/ig, function (matched, $1) { return `copyText('${$1}')` })
-                    vm.docs.push(element)
-                    currentPage = page - 1;
-                    $btn.prop('disabled', false)
-                    $loadingHint.addClass('hidden')
-                    window.scroll(0, 0);
-                });
+                composeResource(res)
+                currentPage = page - 1;
+                setValue()
             })
         }
     })
@@ -187,16 +112,50 @@ connectMongo(function (db) {
 
 function getBundles(offset) {
     return new Promise((resolve, reject) => {
-        Store.paginate({}, { sort:'-insertDate', offset, limit: 15 }, function (err, result) {
+        Store.paginate({}, { sort: '-insertDate', offset, limit: 15 }, function (err, result) {
             if (err) { reject(err) } else { resolve(result) }
         })
     })
 }
 
+function composeResource(res) {
+    // 检测页数 
+    const tmpPages = res.total / 15
+    if (totals !== tmpPages) {
+        totals = tmpPages
+        $tpages.text(`共${Math.ceil(totals)}頁`)
+    }
+    // 
+    res.docs.forEach(element => {
+        const $body = $(element.info)
+        const p = $body.find("p")
+        const star = $body.find('.star-name>a')
+        let htmlStr = ''
+        element.info = ''
+        for (let i = 0; i < 3; i++) {
+            element.info += p[i].outerHTML.replace(/style=\"[\s\S]*?\"/ig, '')
+        }
+        if (star.length) {
+            star.each((index, ele) => { htmlStr += `<span> ${ele.innerHTML} </span>` })
+        } else {
+            htmlStr = ` <span> 未知 </span>`
+        }
+        element.info += `<p><span>演員:</span>${htmlStr}</p>`
+        element.magnet = element.magnet.replace(/&apos;/g, `'`).replace(/window\.open\(\'([\s\S]*?)\',\'_self\'\)/ig, function (matched, $1) { return `copyText('${$1}')` })
+        // console.log(element.magnet)
+        vm.docs.push(element)
+    });
+}
+
+function setValue() {
+    localStorage.setItem('pages', currentPage)
+    $btn.prop('disabled', false)
+    $loadingHint.addClass('hidden')
+    window.scroll(0, 0);
+}
+
 window.copyText = function copyText(text) {
-    copy.value = text
-    copy.select()
-    document.execCommand('copy')
+    clipboard.writeText(unescape(text))
     $copyHint.removeClass('hidden')
     setTimeout(() => {
         $copyHint.addClass('hidden')
